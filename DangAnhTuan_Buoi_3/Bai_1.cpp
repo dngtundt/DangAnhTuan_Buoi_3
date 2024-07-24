33238310690cfcf3e161b771a2a7276b58779c18
dngtundt <dtun11111@gmail.com>
2024-07-24 13:49:07 +0700

    BT 1: thêm hàm tạo ma trận ngẫu nhiên và hàm xuất ma trận

---
+++ DangAnhTuan_Buoi_3/Bai_1.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+
+void taoMaTranNgauNhien(int a[MAX][MAX], int m, int n, int k) {
+    srand(time(0));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = rand() % (k + 1);
+        }
+    }
+}
+
+void xuatMaTran(int a[MAX][MAX], int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%4d", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int a[MAX][MAX];
+    int m, n, k;
+    int choice;
+
+    do {
+        printf("\nMENU\n");
+        printf("1. Tao va xuat ma tran ngau nhien\n");
+        printf("0. Thoat\n");
+        printf("Nhap lua chon cua ban: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Nhap so dong m: ");
+            scanf("%d", &m);
+            printf("Nhap so cot n: ");
+            scanf("%d", &n);
+            printf("Nhap gia tri k: ");
+            scanf("%d", &k);
+            taoMaTranNgauNhien(a, m, n, k);
+            xuatMaTran(a, m, n);
+            break;
+
+        case 0:
+            printf("Thoat chuong trinh.\n");
+            break;
+        default:
+            printf("Lua chon khong hop le. Vui long chon lai.\n");
+        }
+    } while (choice != 0);
+
+    return 0;
+}
