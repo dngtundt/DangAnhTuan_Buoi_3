71d26d58c2fd7f85eb02a83891127a40ef79a492
dngtundt <dtun11111@gmail.com>
2024-07-24 15:01:48 +0700

    BT 2: Tìm phần tử lớn nhất thuộc tam giác trên và sắp xếp zigzac

--- DangAnhTuan_Buoi_3/Bai_2.cpp
+++ DangAnhTuan_Buoi_3/Bai_2.cpp
@@ -43,6 +43,54 @@ void xuatPhanTuSongSongDuongCheoChinh(int a[MAX][MAX], int n) {
     printf("\n");
 }
 
+
+int timPhanTuLonNhatTamGiacTren(int a[MAX][MAX], int n) {
+    int max = a[0][1];
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i][j] > max) {
+                max = a[i][j];
+            }
+        }
+    }
+    return max;
+}
+
+void sapXepzigzag(int a[MAX][MAX], int n) {
+    int temp[MAX * MAX];
+    int k = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[k++] = a[i][j];
+        }
+    }
+
+    for (int i = 0; i < k - 1; i++) {
+        for (int j = i + 1; j < k; j++) {
+            if (temp[i] > temp[j]) {
+                int swap = temp[i];
+                temp[i] = temp[j];
+                temp[j] = swap;
+            }
+        }
+    }
+
+    k = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = temp[k++];
+            }
+        }
+        else {
+            for (int j = n - 1; j >= 0; j--) {
+                a[i][j] = temp[k++];
+            }
+        }
+    }
+}
+
 int main() {
     int a[MAX][MAX];
     int n;
@@ -63,6 +111,8 @@ int main() {
         printf("0. Thoat\n");
         printf("1. Xuat cac phan tu tren duong cheo chinh\n");
         printf("2. Xuat cac phan tu thuoc duong cheo song song voi duong cheo chinh\n");
+        printf("3. Tim phan tu max thuoc tam giac tren cua duong cheo chinh\n");
+        printf("4. Sap xep ma tran tang dan theo kieu zic-zac\n");
         printf("Chon: ");
         scanf("%d", &choice);
 
@@ -75,6 +125,14 @@ int main() {
         case 2:
             xuatPhanTuSongSongDuongCheoChinh(a, n);
             break;
+        case 3:
+            printf("Phan tu max thuoc tam giac tren cua duong cheo chinh: %d\n", timPhanTuLonNhatTamGiacTren(a, n));
+            break;
+        case 4:
+            sapXepzigzag(a, n);
+            xuatMaTran(a, n);
+            break;
+
         default:
             printf("Lua chon khong hop le!\n");
             break;
